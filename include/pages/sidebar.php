76d9062ec7f8050847bf34a26e7db862d75c020f
amranhakim10 <amranhakim9@gmail.com>
2023-05-31 23:19:38 +0700

    Initial commit

--- include/pages/sidebar.php
+++ include/pages/sidebar.php
@@ -1,8 +1,8 @@
 <div class="left-side-bar">
     <div class="brand-logo">
         <a href="index.html">
-            <img src="vendors/images/deskapp-logo.svg" alt="" class="dark-logo">
-            <img src="vendors/images/deskapp-logo-white.svg" alt="" class="light-logo">
+            <img src="assets/vendors/images/deskapp-logo.svg" alt="" class="dark-logo">
+            <img src="assets/vendors/images/deskapp-logo-white.svg" alt="" class="light-logo">
         </a>
         <div class="close-sidebar" data-toggle="left-sidebar-close">
             <i class="ion-close-round"></i>
@@ -191,7 +191,7 @@
                 <li>
                     <a href="https://dropways.github.io/deskapp-free-single-page-website-template/" target="_blank" class="dropdown-toggle no-arrow">
                         <span class="micon dw dw-paper-plane1"></span>
-                        <span class="mtext">Landing Page <img src="vendors/images/coming-soon.png" alt="" width="25"></span>
+                        <span class="mtext">Landing Page <img src="assets/vendors/images/coming-soon.png" alt="" width="25"></span>
                     </a>
                 </li>
             </ul>
