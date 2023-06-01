70ce43cfd6656cbf19f81bd01155a0e676ec90b4
amranhakim10 <amranhakim9@gmail.com>
2023-06-01 23:11:46 +0700

    Initial commit

--- pages/general-section/search.php
+++ pages/general-section/search.php
@@ -1,4 +1,24 @@
 <title>Search <?= $nameTitle; ?></title>
+<?php
+// FUNCTION SEARCHING
+$findInputREFTN = '';
+$findInputTypeREFTN = '';
+$resultreftn = 'none';
+if (isset($_GET['findone'])) {
+    $findInputREFTN = $_GET['findInputREFTN'];
+    $findInputTypeREFTN = $_GET['findInputTypeREFTN'];
+    $resultreftn = 'show';
+}
+
+$findInputAJU = '';
+$findInputTypeAJU = '';
+$resultaju = 'none';
+if (isset($_GET['findtwo'])) {
+    $findInputAJU = $_GET['findInputAJU'];
+    $findInputTypeAJU = $_GET['findInputTypeAJU'];
+    $resultaju = 'show';
+}
+?>
 <div class="main-container">
     <!-- Page Title -->
     <div class="up-page-title">
@@ -32,6 +52,210 @@
     <!-- End Page Title -->
     <div class="xs-pd-20-10 pd-ltr-20">
         <!-- UI -->
+        <!-- Search -->
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <i class="icon-copy dw dw-filter1"></i> Filter Data - by
+                        <select type="text" id="findby" style="background: transparent;border-color: transparent;">
+                            <option value="opone">REF/TN</option>
+                        </select>
+                    </div>
+                    <div class="panel-body">
+                        <div class="page-add">
+                            <form method="get" action="search.php" id="fformone" style="display: show;">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Ref / Tn </label>
+                                            <?php if ($findInputREFTN == '') { ?>
+                                                <input type="text" name="findInputREFTN" id="idfindInputREFTN" class="form-control" placeholder="Input Ref / Tn..." required>
+                                            <?php } else { ?>
+                                                <input type="text" name="findInputREFTN" id="idfindInputREFTN" class="form-control" placeholder="Input Ref / Tn..." value="<?= $findInputREFTN; ?>">
+                                            <?php } ?>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Type</label>
+                                            <select type="text" name="findInputTypeREFTN" id="idfindInputTypeREFTN" class="form-control" required>
+                                                <?php if ($findInputTypeREFTN == '') { ?>
+                                                    <option value="">-- Select Type --</option>
+                                                <?php } else { ?>
+                                                    <option value="<?= $findInputTypeREFTN; ?>"><?= $findInputTypeREFTN; ?></option>
+                                                    <option value="">-- Select Type --</option>
+                                                <?php } ?>
+                                                <option value="import">Import</option>
+                                                <option value="export">Export</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12" style="text-align: right;">
+                                        <a href="search.php" type="button" class="btn btn-info">
+                                            <div class="icon-btn-y">
+                                                <i class="icon-copy dw dw-delete"></i>
+                                                &nbsp;Cancel
+                                            </div>
+                                        </a>
+                                        <button type="submit" name="findone" id="idbtnfindone" class="btn btn-primary"><i class="icon-copy dw dw-search"></i> Search</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <i class="icon-copy dw dw-filter1"></i> Filter Data - by
+                        <select type="text" id="findby" style="background: transparent;border-color: transparent;">
+                            <option value="opone">Aju</option>
+                        </select>
+                    </div>
+                    <div class="panel-body">
+                        <div class="page-add">
+                            <form method="get" action="search.php" id="fformone" style="display: show;">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Aju </label>
+                                            <?php if ($findInputAJU == '') { ?>
+                                                <input type="text" name="findInputAJU" id="idfindInputAJU" class="form-control" placeholder="Input Aju..." required>
+                                            <?php } else { ?>
+                                                <input type="text" name="findInputAJU" id="idfindInputAJU" class="form-control" placeholder="Input Aju..." value="<?= $findInputAJU; ?>">
+                                            <?php } ?>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Type</label>
+                                            <select type="text" name="findInputTypeAJU" id="idfindInputTypeAJU" class="form-control" required>
+                                                <?php if ($findInputTypeAJU == '') { ?>
+                                                    <option value="">-- Select Type --</option>
+                                                <?php } else { ?>
+                                                    <option value="<?= $findInputTypeAJU; ?>"><?= $findInputTypeAJU; ?></option>
+                                                    <option value="">-- Select Type --</option>
+                                                <?php } ?>
+                                                <option value="import">Import</option>
+                                                <option value="export">Export</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12" style="text-align: right;">
+                                        <a href="search.php" type="button" class="btn btn-info">
+                                            <div class="icon-btn-y">
+                                                <i class="icon-copy dw dw-delete"></i>
+                                                &nbsp;Cancel
+                                            </div>
+                                        </a>
+                                        <button type="submit" name="findtwo" id="idbtnfindtwo" class="btn btn-primary"><i class="icon-copy dw dw-search"></i> Search</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Search -->
         <!-- UI -->
     </div>
 </div>
+<!-- Consignee -->
+<script type="text/javascript">
+    // Input - Add
+    if (window?.location?.href?.indexOf('CaddSuccess') > -1) {
+        Swal.fire({
+            title: 'Success Alert!',
+            icon: 'success',
+            text: 'Data saved successfully!',
+        })
+        history.replaceState({}, '', './search.php');
+    }
+
+    if (window?.location?.href?.indexOf('CaddFailed') > -1) {
+        Swal.fire({
+            title: 'Failed Alert!',
+            icon: 'error',
+            text: 'Data failed to save, please contact your administrator!',
+        })
+        history.replaceState({}, '', './search.php');
+    }
+
+    if (window?.location?.href?.indexOf('CaddReady') > -1) {
+        Swal.fire({
+            title: 'Failed Alert!',
+            icon: 'error',
+            text: 'Consignee Name already registered, please contact your administrator!',
+        })
+        history.replaceState({}, '', './search.php');
+    }
+    // End Input - Add
+
+    // Update Data
+    if (window?.location?.href?.indexOf('CUpdateSuccessCC') > -1) {
+        Swal.fire({
+            title: 'Success Alert!',
+            icon: 'success',
+            text: 'Data updated successfully!',
+        })
+        history.replaceState({}, '', './search.php');
+    }
+
+    if (window?.location?.href?.indexOf('CUpdateFailed') > -1) {
+        Swal.fire({
+            title: 'Failed Alert!',
+            icon: 'error',
+            text: 'Data failed to updated, please contact your administrator!',
+        })
+        history.replaceState({}, '', './search.php');
+    }
+
+    if (window?.location?.href?.indexOf('CUpdateReady') > -1) {
+        Swal.fire({
+            title: 'Failed Alert!',
+            icon: 'error',
+            text: 'Consignee Name already registered, please contact your administrator!',
+        })
+        history.replaceState({}, '', './search.php');
+    }
+    // End Update Data
+
+    // Delete
+    if (window?.location?.href?.indexOf('DeleteSuccess') > -1) {
+        Swal.fire({
+            title: 'Delete Alert!',
+            icon: 'info',
+            text: 'Data delete successfully!',
+        })
+        history.replaceState({}, '', './search.php');
+    }
+
+    if (window?.location?.href?.indexOf('DeleteFailed') > -1) {
+        Swal.fire({
+            title: 'Delete Alert!',
+            icon: 'info',
+            text: 'Data failed to delete, please contact your administrator!',
+        })
+        history.replaceState({}, '', './search.php');
+    }
+    // End Delete
+
+    // REF IMPORT
+    function openDetails(evt, cityName) {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        document.getElementById(cityName).style.display = "block";
+        evt.currentTarget.className += " active";
+    }
+    // END REF IMPORT
+</script>
